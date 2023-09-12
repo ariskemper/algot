7d51b3d543fed955c05a8692de13dee05fc0ae49
Aris Kemper <aris.github@gmail.com>
2023-09-12 12:42:03 +0200

    Add counting sort algorithm (#4)

--- src/sort/index.ts
+++ src/sort/index.ts
@@ -1,4 +1,5 @@
 import { BubbleSortFn, bubbleSort } from './bubble/bubble'
+import { countingSort, CountingSortFn } from './counting/counting'
 import { HeapSortFn, heapSort} from './heap/heap'
 import { InsertionSortFn, insertionSort } from './insertion/insertion'
 import { MergeSortFn, mergeSort } from './merge/merge'
@@ -10,10 +11,13 @@ export type SortFn = BubbleSortFn |
   MergeSortFn |
   SelectionSortFn
 
+export type IntegerSortFn  = CountingSortFn
+
 export {
   bubbleSort,
   heapSort,
   insertionSort,
   mergeSort,
-  selectionSort
+  selectionSort,
+  countingSort
 }
--- src/utils/test-sort.ts
+++ src/utils/test-sort.ts
@@ -1,27 +1,47 @@
-import { SortFn } from '../sort'
+import { IntegerSortFn, SortFn } from '../sort'
 
-export function testSort(fn: SortFn ) {
-  describe(`${fn}`, () => {
-    test('It should sort strings', () => {
-      const input = ['banana', 'apple', 'čokolada', 'cherry', 'date', 'elderberry', 'fig']
+export function testSort(fn: SortFn) {
+  testSortStrings(fn)
+  testSortIntegers(fn)
+}
+
+function testSortStrings(fn: SortFn): void {
+  describe(`${fn.name}`, () => {
+    it('It should sort strings', () => {
+      const input = ['banana', 'apple', 'čoko', 'cherry', 'date', 'elderberry', 'fig']
       const result = fn(input)
 
       expect(result).toEqual([
         'apple',
         'banana',
         'cherry',
-        'čokolada',
+        'čoko',
         'date',
         'elderberry',
         'fig'
       ])
     })
+  })
+}
 
-    test('It should sort numbers', () => {
+export function testSortIntegers(fn: IntegerSortFn): void {
+  describe(`${fn.name}`, () => {
+    test('It should sort integer numbers', () => {
       const input = [5, 2, 4, 6, 1, 3, 7, 10, 9, 8]
       const result = fn(input)
 
       expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    })
+  })
+}
+
+export function testSortSingleInteger(fn: IntegerSortFn): void {
+  describe(`${fn.name}`, () => {
+    test('It should return same array', () => {
+      const input = [5]
+      const result = fn(input)
+
+      expect(result).toEqual([5])
     })
   })
 }
