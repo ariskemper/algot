04220d304f9493a8628b19e11e02c64600df17ca
Aris Kemper <aris.github@gmail.com>
2023-09-18 16:54:36 +0200

    refactor: replace jest with node test runner

--- src/utils/compare.spec.ts
+++ src/utils/compare.spec.ts
@@ -1,8 +1,11 @@
+import assert from 'node:assert/strict'
+import { describe, it } from 'node:test'
 import { compare } from './compare'
 
 describe('compare', () => {
-  test('It should thrown an error', () => {
-    expect(() => compare<number | string>(1, 'banana')).toThrow(
+  it('It should thrown an error', () => {
+    assert.throws(
+      () => compare<number | string>(1, 'banana'),
       new Error('Incompatible types for comparison')
     )
   })
