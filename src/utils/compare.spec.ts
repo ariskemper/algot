2576f29d57cb264baa89e2c844f40f6307b95d1c
Aris Kemper <aris.github@gmail.com>
2023-11-04 21:24:28 +0100

    feat: cube sort algorithm with tests (#41)

--- src/utils/compare.spec.ts
+++ src/utils/compare.spec.ts
@@ -4,7 +4,7 @@ import { describe, it } from 'node:test'
 import { compare } from './compare'
 
 describe('compare', () => {
-  it('It should thrown an error', () => {
+  it('should thrown an error', () => {
     assert.throws(() => compare<number | string>(1, 'banana'), new Error('Incompatible types for comparison'))
   })
 })
